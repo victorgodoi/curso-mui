1b20ab8567e3e948971a157662b334696920388b
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2024-04-08 10:10:45 -0300

    Add Material UI 5 (MUI)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 const AppRoutes = () => {
 
   return(
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+      <Route path="/pagina-inicial" element={<Button>Test</Button>} />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   )
