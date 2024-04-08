3bae7f7e97e8e75078be3ec9532f06bc675596a5
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2024-04-08 15:57:46 -0300

    Adicionado Menu Lateral

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { AppThemeProvider } from "./shared/contexts";
+import { MenuLateral } from "./shared/components";
+
 
 
 export function App() {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <AppRoutes />
+        <MenuLateral>
+          <AppRoutes />
+        </MenuLateral>
       </BrowserRouter>
     </AppThemeProvider>
   );
---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,42 @@
+import { Height, WidthFull } from "@mui/icons-material";
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material"
+import { Box } from "@mui/system"
+
+interface IMenuLateralProps {
+  children: React.ReactNode
+}
+
+export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
+
+  const theme = useTheme();
+
+  return(
+    <>
+      <Drawer variant="permanent">
+        <Box width={theme.spacing(28)} display="flex" flexDirection="column" height="100%">
+          <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+            <Avatar
+              sx={{height: theme.spacing(12), width: theme.spacing(12)}}
+              // src="https://img.freepik.com/vetores-gratis/modelo-de-logotipo-de-computador-de-tecnologia_23-2149204144.jpg?w=360"
+              src="https://img.freepik.com/vetores-gratis/modelo-de-logotipo-de-computador-de-tecnologia_23-2149199401.jpg" 
+            />
+          </Box>
+          <Divider />
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página Inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+      <Box height="vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  )
+}
