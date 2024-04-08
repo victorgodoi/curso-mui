3d3bf08033506d3ac5c4ddb98ea472ecdefa7407
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2024-04-08 09:46:48 -0300

    Adicionado o React-Router-Dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      Teste
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-export { };
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const AppRoutes = () => {
+
+  return(
+    <Routes>
+      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes;
